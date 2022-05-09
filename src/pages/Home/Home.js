19b8209f77bfdcfedfc9626fbc67019ca6584b42
masudx08 <promasud225@gmail.com>
2022-05-10 05:18:52 +0600

    structure don

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,8 +3,11 @@ import MyNavbar from "../../components/MyNavbar/MyNavbar";
 import "./home.css";
 import HightlightImg from "../../assets/highlightImg.png";
 import LiveStreamSS from "../../assets/livestream.png";
+import Logo from "../../assets/logo.png";
+
 import { Col, Container, Row } from "react-bootstrap";
 import MyCarousel from "../../components/MyCarousel/MyCarousel";
+
 export default function Home() {
   return (
     <div>
@@ -33,10 +36,18 @@ export default function Home() {
           Rewatch your favorite game highlights <br /> on any device
         </h3>
         <div className="blue-color">
-          <a href="">Instant Share</a>
-          <a href="">Replay</a>
-          <a href="">4K Ultra HD</a>
-          <a href="">Watch Anywhere</a>
+          <div>
+            <a href="">Instant Share</a>
+          </div>
+          <div>
+            <a href="">Replay</a>
+          </div>
+          <div>
+            <a href="">4K Ultra HD</a>
+          </div>
+          <div>
+            <a href="">Watch Anywhere</a>
+          </div>
         </div>
         <div>
           <img style={{ maxWidth: "90%" }} src={HightlightImg} alt="" />
@@ -93,15 +104,72 @@ export default function Home() {
       </div>
 
       {/* Carousel  */}
-      <div className='carouselCont'>
+      <div className="carouselCont">
         <MyCarousel />
       </div>
 
       {/* Signup  */}
-      <div className="signupCont"></div>
+      <div className="signupCont">
+        <h4>Get Started Today</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <button>SIGN UP</button>
+      </div>
 
       {/* Footer  */}
-      <div className="footerCont"></div>
+      <div className="footerCont">
+        <Container>
+          <Row>
+            <Col md={2}>
+              <img src={Logo} alt="" />
+              <p>4517 Washington Ave. Manchester, Kentucky 39495</p>
+            </Col>
+            <Col md={2}>
+              <h6>Quick Links</h6>
+              <div>
+                <a href="#">Home</a>
+              </div>
+              <div>
+                <a href="#">About CrickAi</a>
+              </div>
+              <div>
+                <a href="#">Our Services</a>
+              </div>
+              <div>
+                <a href="#">Features</a>
+              </div>
+              <div>
+                <a href="#">Contact Us</a>
+              </div>
+            </Col>
+            <Col md={2}>
+              <h6>Legal</h6>
+              <div>
+                <a href="#">Privacy Policy</a>
+              </div>
+              <div>
+                <a href="#">Terms and Conditions</a>
+              </div>
+            </Col>
+            <Col md={2}>
+              <h6>Contact Us</h6>
+              <div>
+                <a href="#">example@gmail.com</a>
+              </div>
+              <div>
+                <a href="#">(239) 555-0108</a>
+              </div>
+            </Col>
+            <Col md={4}>
+              <h6>Newsletter</h6>
+              <p>Never miss an update. Subscribe to our newsletter.</p>
+              <div className="subscribeCont">
+                <input type="text" placeholder="Email" />
+                <h6 className="subscribeBtn">Subscribe</h6>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
